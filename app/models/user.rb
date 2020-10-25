56c00a396c6d6357b2592b2b40d76a152de72cfa
TechMura <mura.sk.katu@gmail.com>
2020-10-25 17:33:29 +0900

    userモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
+  validates :nickname, presence: true, uniqueness: true
+  validates :first_name, presence: true,format: {with: NAME_REGEX }
+  validates :last_name, presence: true,format: {with: NAME_REGEX }
+  validates :first_name_kana, presence: true,format: {with: NAME_KANA_REGEX }
+  validates :last_name_kana, presence: true,format: {with: NAME_KANA_REGEX }
   validates :birthday, presence: true
 
   
