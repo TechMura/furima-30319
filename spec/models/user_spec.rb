139228014f724a0e441a0b0e567b5823c451155b
TechMura <mura.sk.katu@gmail.com>
2020-10-25 17:33:11 +0900

    rspecの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "emailが空では登録出来ない" do
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      
+    end
+
+  end
+end
