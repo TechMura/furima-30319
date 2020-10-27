fee07f8236ae68cd4cad08053c0d6ed37d31c4d3
TechMura <mura.sk.katu@gmail.com>
2020-10-27 19:17:02 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,14 @@ class User < ApplicationRecord
 
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
 
+  validates :password, length:{minimum:6}, format:{ with: PASSWORD_REGEX }
   validates :nickname, presence: true, uniqueness: true
   validates :first_name, presence: true,format: {with: NAME_REGEX }
   validates :last_name, presence: true,format: {with: NAME_REGEX }
   validates :first_name_kana, presence: true,format: {with: NAME_KANA_REGEX }
   validates :last_name_kana, presence: true,format: {with: NAME_KANA_REGEX }
   validates :birthday, presence: true
-
   
 end
