fe73aabc1906e3fc14864adaaef5d7b6d5d7a360
TechMura <mura.sk.katu@gmail.com>
2020-10-31 21:05:59 +0900

    "---"の表示をpromptで変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
 
   validates :name,                presence: true, length: {maximum: 8}
   validates :explanation,         presence: true, length: {maximum: 1000}
-  validates :category_id,         presence: true, numericality: { other_than: 1 }
-  validates :status_id,           presence: true, numericality: { other_than: 1 }
-  validates :delivery_payment_id,  presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id,       presence: true, numericality: { other_than: 1 }
-  validates :shipping_period_id,  presence: true, numericality: { other_than: 1 }
+  validates :category_id,         presence: true
+  validates :status_id,           presence: true
+  validates :delivery_payment_id,  presence: true
+  validates :prefecture_id,       presence: true
+  validates :shipping_period_id,  presence: true
   validates :price,               presence: true, numericality: {only_integer: true,greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
   
   belongs_to :user
