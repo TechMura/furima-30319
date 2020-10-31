175829442b848798ba39dc8ba3364042100cd519
TechMura <mura.sk.katu@gmail.com>
2020-10-31 14:53:16 +0900

    Activestorageの導入

--- Gemfile
+++ Gemfile
@@ -68,7 +68,9 @@ gem 'rails-i18n'
 
 gem 'active_hash'
 
+gem 'mini_magick'
 
+gem 'image_processing', '~>1.2'
 
 group :production do
   gem 'rails_12factor'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:explanation,:category_id,:status_id,:delivery_payment_id,:prefecture_id,:shipping_period_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:explanation,:category_id,:status_id,:delivery_payment_id,:prefecture_id,:shipping_period_id,:price).merge(user_id: current_user.id)
   end
 end
