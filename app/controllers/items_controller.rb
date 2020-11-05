53986dc9fec68a285dd5975d086217b10441114f
TechMura <mura.sk.katu@gmail.com>
2020-11-05 17:35:50 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user.id
-      redirect_to root_path 
-    end
+    redirect_to root_path if current_user.id != @item.user.id
   end
 end
