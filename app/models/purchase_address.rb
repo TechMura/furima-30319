4b51f2c4cb7518105e99ad0c3ee920ae249d0744
TechMura <mura.sk.katu@gmail.com>
2020-11-08 17:28:55 +0900

    クレジット意外と購入情報の登録実装

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :building_name
+    validates :phone_number
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, phone_number: phone_number, purchase_id: purchase.id)
+  end
+
+end
