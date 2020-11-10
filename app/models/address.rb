cc7d93aa22f9e363661fbbcfff47cdb264cfc4f7
TechMura <mura.sk.katu@gmail.com>
2020-11-10 17:42:39 +0900

    addressモデルの不要なコメントを削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,3 @@
 class Address < ApplicationRecord
-  # with_options presence: ture do
-  #   validates :postal_code
-  #   validates :prefecture_id
-  #   validates :city
-  #   validates :house_number
-  #   validates :building_name
-  #   validates :phone_number
-  # end
-
   belongs_to :purchase
 end
