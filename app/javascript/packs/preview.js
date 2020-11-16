284aa5390c925cd008d39f85b277626eb76ad29b
TechMura <mura.sk.katu@gmail.com>
2020-11-16 21:05:24 +0900

    クラス名変更

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,14 +1,14 @@
 if (document.URL.match(/new/) || document.URL.match(/edit/)){
   document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+    const ImageList = document.getElementById('image-lists');
 
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
       
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
-      blobImage.setAttribute('id', 'image-item')
-      blobImage.setAttribute('class', 'image-item')
+      blobImage.setAttribute('id', 'image-list')
+      blobImage.setAttribute('class', 'image-list')
 
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
@@ -17,7 +17,7 @@ if (document.URL.match(/new/) || document.URL.match(/edit/)){
 
     document.getElementById('item-image').addEventListener('change',function(e){
 
-      const imageContent = document.getElementById('image-item');
+      const imageContent = document.getElementById('image-list');
       if (imageContent){
         imageContent.remove();
       }
